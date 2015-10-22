f10f83d144fe47e088a67d65324d44189d242644
Francisco Guzman <fguzman@qf.org.qa>
2015-10-22 17:39:15 +0300

    fixed problems with inconsistent normalization

--- scripts/stream/segment-phrase_boundary.sh
+++ scripts/stream/segment-phrase_boundary.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 INPUT="$1"
-KEYFILE="$3"
-OUTPUT="$2"
-OTHER="$4"
+CHUNKED="$2"
+KEYFILE="$4"
+OUTPUT="$3"
+OTHER="$5"
 
 
 # put segment the input
 #perl  /work/stream/scripts/AMIRA_NOTOK_NONORM.pl config=/work/stream/scripts/amiraconfig.atb.bpc.utf8 file=$INPUT
 
 # produce segmentation
-python /work/stream/scripts/getPB-LEX.py $INPUT $OTHER
+python /work/stream/scripts/getPB-LEX.py $CHUNKED $INPUT $OTHER
 
 # produce keyfile
-mv $INPUT.segs.idx $KEYFILE
+mv $CHUNKED.segs.idx $KEYFILE
 
 # produce textfile
-mv $INPUT.segs.txt $OUTPUT
+mv $CHUNKED.segs.txt $OUTPUT
