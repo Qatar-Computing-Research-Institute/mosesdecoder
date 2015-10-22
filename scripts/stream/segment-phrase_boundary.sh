8bb4fca99b11b56eaf28706e2a386c3fda803729
Francisco Guzman <fguzman@qf.org.qa>
2015-10-22 17:58:40 +0300

    modified segmentpb

--- scripts/stream/segment-phrase_boundary.sh
+++ scripts/stream/segment-phrase_boundary.sh
@@ -11,7 +11,7 @@ OTHER="$5"
 #perl  /work/stream/scripts/AMIRA_NOTOK_NONORM.pl config=/work/stream/scripts/amiraconfig.atb.bpc.utf8 file=$INPUT
 
 # produce segmentation
-python /work/stream/scripts/getPB-LEX.py $CHUNKED $INPUT $OTHER
+python /work/moses-stream/scripts/stream/getPB-LEX.py $CHUNKED $INPUT $OTHER
 
 # produce keyfile
 mv $CHUNKED.segs.idx $KEYFILE
