69945cd1db6d676ca95d8ddf9e36eceb2c7177fa
Francisco Guzman <fguzman@qf.org.qa>
2015-10-26 17:42:09 +0300

    fixed px chunking

--- scripts/stream/chunk-PX.sh
+++ scripts/stream/chunk-PX.sh
@@ -5,9 +5,9 @@ OUTPUT="$2"
 
 
 # put segment the input
-python segment-given-phrases.py /work/stream/ems/evaluation/iwslt_all.gz $INPUT >$OUTPUT
+python /work/moses-stream/scripts/stream/segment-given-phrases.py /work/stream/ems/evaluation/iwslt_all.gz $INPUT >$OUTPUT
 
 
 
 # produce textfile
-mv $INPUT.amirabpc $OUTPUT
+#mv $INPUT.amirabpc $OUTPUT
