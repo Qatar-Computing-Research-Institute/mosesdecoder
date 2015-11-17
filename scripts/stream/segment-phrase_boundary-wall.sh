33405165fcb6360a22fc7edca0c0997373af7554
Francisco Guzman <fguzman@qf.org.qa>
2015-11-17 10:54:02 +0300

    added segmentation with wall scripts

---
+++ scripts/stream/segment-phrase_boundary-wall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INPUT="$1"
+CHUNKED="$2"
+OUTPUT="$3"
+KEYFILE="$4"
+OTHER="$5"
+
+
+# put segment the input
+#perl  /work/stream/scripts/AMIRA_NOTOK_NONORM.pl config=/work/stream/scripts/amiraconfig.atb.bpc.utf8 file=$INPUT
+
+# produce segmentation
+python /work/moses-stream/scripts/stream/getPB-LEX-wall.py $CHUNKED $INPUT $OTHER
+
+# produce keyfile
+mv $CHUNKED.segs.idx $KEYFILE
+
+# produce textfile
+mv $CHUNKED.segs.txt $OUTPUT
